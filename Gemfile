6a5ed84f59321736aced5b6c243d5ad5f4256fa3
Pawel Licznar <pawellicznar@gmail.com>
2017-03-12 01:31:58 +0100

    Add simple_form gem

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem "haml-rails", "~> 0.9"
 gem 'bootstrap-sass'
+gem 'simple_form'
 
 gem 'devise'
 gem 'decent_exposure', '3.0.0'
--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -2,10 +2,21 @@ class AnnouncementsController < ApplicationController
   expose :announcements, ->{ Announcement.all }
   expose :announcement
 
-  def index
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def create
+    announcement.user_id = current_user.id
+    if announcement.save
+      redirect_to announcement_path(announcement)
+    else
+      render :new
+    end
   end
 
-  def show
+  private
+
+  def announcement_params
+    params.require(:announcement).permit(:title, :description, :anno_type, :animal)
   end
 
 end
