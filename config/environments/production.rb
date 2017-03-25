72d9d17ee41d2c8695082697c864f57604bb367a
Pawel Licznar <pawellicznar@gmail.com>
2017-03-25 22:23:53 +0100

    Configure mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :user_name => ENV["MAIL_USERNAME"],
+    :password => ENV["MAIL_PASSWORD"]
+  }
+  config.action_mailer.perform_deliveries = true
 end
